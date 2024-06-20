c3a09db7c4baaca174b4a545fbbc4fe018a0a3d2
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2024-06-20 10:46:50 +0700

    Create Saving.java

---
+++ Saving.java
@@ -0,0 +1,15 @@
+public class Saving extends Account{
+    private static String accountType= "Saving";
+    Saving(double initialDeposit){
+        super();
+        this.setBalance(initialDeposit);
+        this.checkInterest(0);
+    }
+    @Override
+    public String toString(){
+        return "Account Type:"+ accountType + "Account\n"+
+                "Account Number:"+ this.getAccountNumber() + "\n"+
+                "Balance" + this.getBalance()+ "\n"+
+                "Interest Rate:" + this.getInterest()+ "%\n";
+    }
+}
