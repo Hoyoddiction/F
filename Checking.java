49e4a499451265ee5f8a1387feaa54f24fea116f
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2024-06-20 22:42:26 +0700

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,15 +1,20 @@
 public class Checking extends Account {
-    private static String accountType= "Checking";
-    Checking(double initialDeposit){
-        super();
-        this.setBalance(initialDeposit);
-        this.checkInterest(0);
+    // Static variable to store the account type, common to all Checking accounts
+    private static String accountType = "Checking";
+
+    // Constructor that takes an initial deposit as a parameter
+    Checking(double initialDeposit) {
+        super(); // Call the constructor of the superclass (Account)
+        this.setBalance(initialDeposit); // Set the initial balance
+        this.checkInterest(0); // Check if interest should be applied
     }
+
+    // Overriding the toString method to provide a string representation of the Checking account details
     @Override
-    public String toString(){
-        return "Account Type:"+ accountType + "Account\n"+
-                "Account Number:"+ this.getAccountNumber() + "\n"+
-                "Balance" + this.getBalance()+ "\n"+
-                "Interest Rate:" + this.getInterest() + "%\n";
+    public String toString() {
+        return "Account Type: " + accountType + " Account\n" + // Account type
+                "Account Number: " + this.getAccountNumber() + "\n" + // Account number
+                "Balance: " + this.getBalance() + "\n" + // Account balance
+                "Interest Rate: " + this.getInterest() + "%\n"; // Interest rate
     }
 }
